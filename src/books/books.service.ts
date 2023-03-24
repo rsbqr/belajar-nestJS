5441421438f516181f8546625f907a2e47cb7d8d
rsbqr <rsbqr.coder@gmail.com>
2023-03-24 19:08:26 +0700

    service books tambahin return

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+    private books:any[]=[];
+
+    getAllBooks():any[]{
+        return this.books;
+    }
+}
